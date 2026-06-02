a9684b17dcb2250dad8b8381a3dd3261a95f02be
Bored008 <dahiyahimanshu80@gmail.com>
2026-06-02 12:45:27 +0530

    fixed watch svg typo

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -221,7 +221,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div className='shrink-0 w-[90%] md:w-[48%] backdrop-blur-md md:h-fit border border-white/50 rounded-[19px] p-[12px] md:flex items-center text-white md:gap-5 '>
-                    <img src="watch.svg" alt="Watch" className='md:w-[92%] w-full rounded-[12px]' />
+                    <img src="Watch.svg" alt="Watch" className='md:w-[92%] w-full rounded-[12px]' />
                     <div>
                         <div className='flex justify-between'>
                             <div className='text-[24px] font-semibold'>Luxuary Watch WebDesign</div>
